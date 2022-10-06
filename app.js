2306c3459aae678a1827fe8d31f8d1b4468031f3
maciek <42122011+xkcm@users.noreply.github.com>
2022-10-06 15:08:59 +0200

    KP-942 Add Playwright installation and build commands (#4)

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const kaholoPluginLibrary = require("@kaholo/plugin-library");
 
 const playwrightService = require("./playwright-service");
-const autocomplete = require("./autocomplete");
 
-module.exports = kaholoPluginLibrary.bootstrap(
-  {
-    executeDotnetTest: playwrightService.executeDotnetTest,
-  },
-  {
-    listDockerImageTagsAuto: autocomplete.listDockerImageTags,
-  },
-);
+module.exports = kaholoPluginLibrary.bootstrap({
+  executeDotnetTest: playwrightService.executeDotnetTest,
+});
