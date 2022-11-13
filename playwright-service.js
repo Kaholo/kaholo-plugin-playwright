0d13e124094a49925f472562adcf3082089a2ee7
Kenny Marsh <kenny.m@kaholo.io>
2022-11-13 07:11:13 -0800

    [KP-964] relative paths fix

--- playwright-service.js
+++ playwright-service.js
@@ -24,13 +24,13 @@ async function executeDotnetTest(params) {
     throw new Error(`Path ${absoluteProjectPath} does not exist on agent`);
   }
 
-  const fullImageName = `${IMAGE_REPOSITORY}/${IMAGE_NAME}:${imageTag}`;
+  const fullImageName = `${IMAGE_REPOSITORY}/${IMAGE_NAME}:${imageTag ? imageTag : 'latest'}`;
   const commands = [
     "dotnet build &>/var/dotnet-build.log",
     "find ./ -name playwright.ps1 -exec pwsh {} install \\; &>/var/playwright-install.log",
     additionalCommandArguments ? `${DOTNET_TEST_COMMAND} ${additionalCommandArguments}` : DOTNET_TEST_COMMAND,
   ];
-  const projectDirVolumeDefinition = docker.createVolumeDefinition(projectDirectoryPath);
+  const projectDirVolumeDefinition = docker.createVolumeDefinition(absoluteProjectPath);
   const environmentVariables = mapEnvironmentVariablesFromVolumeDefinitions([
     projectDirVolumeDefinition,
   ]);
